787bb60a1840f598f65a2d566c85b40c1fc304ed
Jshtoc <tkdqja0517@gmail.com>
2021-12-04 21:36:32 +0900

    Redirect에 대하여

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter , Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Redirect , Route, Switch} from 'react-router-dom';
 import About from './pages/About';
 import Home from "./pages/Home"
 import Profile from './pages/Profile';
@@ -7,20 +7,24 @@ import Links from './components/Links';
 import NavLinks from './components/NavLinks';
 import Login from './pages/Login';
 
+const isLogin = false;
+
 function App() {
   return (
     <BrowserRouter>
     <Links />
     <NavLinks />
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route 
+          path="/login" 
+          render={()=> (isLogin ? <Redirect to="/" /> : <Login />)}
+        />
         <Route path='/profile/:id' component={Profile} />
         <Route path='/profile' component={Profile} />
         <Route path='/about' component={About} />
         <Route path='/' exact component={Home} />
         <Route component={NotFound} />
       </Switch>
-      
     </BrowserRouter>
   );
 }
